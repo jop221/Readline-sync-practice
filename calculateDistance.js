d647194b2adfe691bd40a99ae1b3c3bf95943811
jop221 <mr.motomot@gmail.com>
2023-11-01 09:43:20 +0300

    a

--- calculateDistance.js
+++ calculateDistance.js
@@ -3,4 +3,4 @@ const  calculateDistance = (x1, y1, x2, y2) => {
     const distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5;
     return distance
 }
-console.log(distance(readlineSync.question('x1: '), readlineSync.question('y1: '), readlineSync.question('x2: '), readlineSync.question('y2: ')));
+console.log(calculateDistance(readlineSync.question('x1: '), readlineSync.question('y1: '), readlineSync.question('x2: '), readlineSync.question('y2: ')));
